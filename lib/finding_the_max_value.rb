27fa323e1a5210f6e877d28b9b64321ace62cd6c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-17 01:29:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   i = 0
   max = array[i]
   while i < array.length - 1 do
-    if array[i] > array[i + 1]
+    if array[i] > array[i + 1] && array[i] > max
       max = array[i]
     end
     i += 1
